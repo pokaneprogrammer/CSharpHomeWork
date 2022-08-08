037c32616d04493a9d9e3a7eb4836afb752508bb
pokaneprogrammer <109076841+pokaneprogrammer@users.noreply.github.com>
2022-08-08 19:36:43 +0300

    Update Program.cs

--- Lesson1Task2/Program.cs
+++ Lesson1Task2/Program.cs
@@ -1,16 +1,16 @@
 ﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-int[]Numbers = { 2, 3, 7 };
+int[] Numbers = { 2, 3, 7 };
 
-for (int i = 0; i < 3; i++)
+int max = Numbers[0];
+for (int i = 1; i < Numbers.Length; i++)
 {
-    int max = Numbers[0];
-
     if (Numbers[i] > max)
     {
         max = Numbers[i];
-        System.Console.WriteLine();
-        System.Console.WriteLine($"Максимальное число {max}");
+
     }
 }
+System.Console.WriteLine();
+System.Console.WriteLine($"Максимальное число {max}");
 System.Console.WriteLine();
